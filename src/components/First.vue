ebc385cc0e8a6330e29ce4bf0d289400ccd6284c
Stella-cmy <2680462740@qq.com>
2020-11-05 11:51:07 +0800

    cmy-first-Login

---
+++ src/components/First.vue
@@ -0,0 +1,59 @@
+<template>
+  <el-container style="height: 500px; border: 1px solid #eee">
+     <el-header style="text-align: center; font-size: 12px;background:pink">
+      header区
+      <i class="el-icon-star-on" style="font-size:20px;color:red" ></i>
+      <!--<el-button type="primary" icon="el-icon-search">搜索</el-button>
+        <el-link href="https://baidu.com" target="_blank">百度一下</el-link>
+      -->
+      <div style="text-align:right">
+        <el-link href="login" target="_blank" >登录</el-link>
+      </div>
+      
+     </el-header>
+     <el-row style="height:300px">
+       <el-col :span="4" >
+         <el-aside  class="left_box" >
+           left -左侧区
+         </el-aside>
+       </el-col>
+       <el-col :span="20">
+          <el-main class="main_box" >
+            hello-main区
+          </el-main>
+       </el-col> 
+     </el-row>
+    
+   
+    <el-footer style="background:lightblue">
+    footer区
+    </el-footer>
+
+  </el-container>
+</template>
+
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ .left_box{
+   width:200px !important;
+   background:#DCDFE6;
+   height:300px;
+ }
+ .main_box{
+   background:lightgreen;
+   height:300px;
+ }
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,71 @@
+<template>
+  <el-container style="height: 700px; border: 1px solid #eee">
+    <el-header class="header_box">
+    <img :src="imgUrl" class="header_left" width="70%" height="200px" align="left" alt="" />
+    <div class="header_right" :span="4">
+    <el-link href="@/login" target="blank">登陆</el-link>
+    <el-link href="@/register" target="blank">注册</el-link>
+    </div>
+    </el-header>
+    <div>
+    <el-row style="height:300px">
+      <el-col :span="4" style="height:100%">
+        <el-aside class="left_box" width="200px">
+        left-左侧区
+        </el-aside>
+      </el-col>
+      <el-col :span="20" style="height:100%">
+        <el-main class="main_box">
+        Hello-Main区
+        </el-main>
+      </el-col>
+    </el-row>
+    <el-footer style="background: lightblue">
+    footer区
+    </el-footer>
+    </div>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      imgUrl: require("@/assets/ljsm.png")
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.left_box{
+background-color: rgb(238, 241, 246);
+height: 300px
+}
+.main_box{
+ background: lightgreen;
+ height:100%
+}
+.header_box{
+  height:500px;
+  width:100%;
+  z-index:-1;
+  position: absolute;
+}
+..header_left{  
+    height:100%;  /**宽高100%是为了图片铺满屏幕 */
+    width:70%;
+    align:left;
+   z-index:1;
+   position: absolute;
+}
+..header_right{
+  height:100%;  /**宽高100%是为了图片铺满屏幕 */
+    width:30%;
+    align:left;
+   z-index:1;
+   position: absolute;
+}
+</style>
